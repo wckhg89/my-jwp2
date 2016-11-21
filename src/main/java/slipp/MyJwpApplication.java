e0cb14a3e31b5132e5f4edb34b6796caee3a4dd2
javajigi <javajigi@gmail.com>
2016-11-21 21:35:48 +0900

    initial commit

---
+++ src/main/java/slipp/MyJwpApplication.java
@@ -0,0 +1,12 @@
+package slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJwpApplication.class, args);
+	}
+}
